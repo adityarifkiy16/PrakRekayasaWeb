aa6fa64b501e08f64c6f0c9ea7b9776ac69bab7b
adityarifkiy16 <adityarifkistemba@gmail.com>
2024-10-05 19:55:06 +0700

    feat: menambah kode iterasi untuk property hobby

--- decode.php
+++ decode.php
@@ -1,17 +1,26 @@
 <?php
-$jsondata = '{  
-    "employee": {  
-        "name": "Aditya Rifki",   
-        "age" : 20,
-        "salary": 56000,   
-        "married": true  
-    }  
+$jsondata = '{
+    "employee": {
+        "name": "Aditya Rifki",
+        "age": 20,
+        "salary": 56000,
+        "married": true,
+        "hobby": ["makan", "tidur", "ngoding"]
+    }
 }';
 
+// Decode the JSON
 $data = json_decode($jsondata);
+
 echo "Name: " . $data->employee->name . "<br>";
 echo "Salary: " . $data->employee->salary . "<br>";
-echo "Age:" . $data->employee->age . "<br>";
+echo "Age: " . $data->employee->age . "<br>";
 
-// check if married is true or false
+// cek status dengan ternary 
 echo "Married: " . ($data->employee->married ? 'Yes' : 'No') . "<br>";
+
+echo "Hobby: " . "<br>";
+// Mengambil data hobby dengan iterasi
+foreach ($data->employee->hobby as $hobby) {
+    echo "- " . $hobby . "<br>";
+}
